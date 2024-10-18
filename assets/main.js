2c515858e68bfce97adabc50802e11aab63b3198
ManuelTrappolini <manuel.trappolini@gmail.com>
2024-10-18 12:02:16 +0200

    add hover effects and pin on pictures

--- assets/main.js
+++ assets/main.js
@@ -21,8 +21,8 @@ axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
         const{url, title} = element
         console.log(url, title);
         const markup = `
-            <div class="card p-relative px-3 py-3 mx-2 mt-4" style="width:18rem;">
-                    <!-- <img class="pin" src="./assets/img/pin.svg" alt=""> -->
+            <div class="card hover p-relative px-3 py-3 mx-2 mt-4" style="width:18rem;">
+                    <img class="pin" src="./assets/img/pin.svg" alt=""> 
                     <img src="${url}"
                         class="" alt="...">
                     <div class="card-body">
