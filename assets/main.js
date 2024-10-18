3996226f2708f1df4ebb256ed4f77c03fae4f7c4
ManuelTrappolini <manuel.trappolini@gmail.com>
2024-10-18 16:30:10 +0200

    add mediquery

--- assets/main.js
+++ assets/main.js
@@ -16,8 +16,6 @@ axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
 .then(response =>{
     generatePictures(response)
     
-    
-    
 }) .catch(error =>console.error(error));
 
 
